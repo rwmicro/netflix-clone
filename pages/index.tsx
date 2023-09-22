96ba2050ee5804fdbc7a91adb4c5bbb3f3400185
rwmicro <xvvrw@proton.me>
2023-09-22 17:49:22 +0200

    [FIX] path ...

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,6 @@
 import Head from "next/head";
 import Header from "../components/main/Header";
 import Search from "../components/index/Search";
-import background from 'public/assets/img/tools/index.jpg'
 
 export default function Accueil() {
   return (
@@ -9,15 +8,15 @@ export default function Accueil() {
       <Head>
         <title>Netflix</title>
       </Head>
-      <div
-        className="hidden sm:block w-full h-screen bg-cover absolute top-0 left-0 index"
-      >
+      <div className="hidden sm:block w-full h-screen bg-cover absolute top-0 left-0 index">
         <Header />
-      <Search />
+        <Search />
       </div>
       <div className="min-h-screen w-full sm:hidden">
         <Header />
-        <p className="absolute left-1/2 top-1/2 text-center -translate-x-1/2 -translate-y-1/2 text-white">Please view this site on a computer</p>
+        <p className="absolute left-1/2 top-1/2 text-center -translate-x-1/2 -translate-y-1/2 text-white">
+          Please view this site on a computer
+        </p>
       </div>
     </>
   );
