70f6750d3b53bbe1808db0a0fbe6d660de86d635
rwmicro <xvvrw@proton.me>
2023-09-22 14:12:13 +0200

    [FIX] index page background path

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 import Head from "next/head";
 import Header from "../components/main/Header";
 import Search from "../components/index/Search";
-import background from '/assets/img/tools/index.jpg'
+import background from 'public/assets/img/tools/index.jpg'
 
 export default function Accueil() {
   return (
