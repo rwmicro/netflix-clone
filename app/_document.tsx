5105f7a3537bf5b2fbba12739c8f1e961f6565a7
rwmicro <xvvrw@proton.me>
2023-11-15 17:37:32 +0100

    [FIX] text-adjustments

--- app/_document.tsx
+++ app/_document.tsx
@@ -4,9 +4,13 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-<link rel="icon" href="favicon.ico" sizes="any" />
+        <link rel="icon" href="favicon.ico" sizes="any" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500&display=swap" rel="stylesheet"/>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500&display=swap"
+          rel="stylesheet"
+        />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </Head>
       <body>
         <Main />
