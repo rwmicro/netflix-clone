9562598ea4e55e4f674bc79ed52023c8669203a0
rwmicro <xvvrw@proton.me>
2024-01-21 02:03:48 +0100

    [ADD] robots.txt to disable all crawnling on the website

--- app/_document.tsx
+++ app/_document.tsx
@@ -10,6 +10,8 @@ export default function Document() {
           rel="stylesheet"
         />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="robots" content="noindex, nofollow" />
+        <meta name="googlebot" content="noindex, nofollow" />
       </Head>
       <body>
         <Main />
